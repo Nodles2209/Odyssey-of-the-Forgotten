398140bd513186a7e29105ecb81a6a799cab7f5c
Linli Cai <cail9@cardiff.ac.uk>
2023-10-18 21:36:09 +0000

    Update printmap.py

--- printmap.py
+++ printmap.py
@@ -4,26 +4,31 @@ testmap = [[2, 1, 3, 1, 4, 1, 9],
        [0, 0, 1, 0, 0, 0, 0],
        [10, 1, 7, 1, 8, 0, 0]]
 
+# the room number and connection state is stored in a 2d matrix
+# room number = (numbers other than '1' and '0') - 1
+# '1' is connected, '0' is disconnect
+
 
 print('\n\n')
 
-for row in range(5):
-    for col in range(7):
+for row in range(5): # max 5 rows
+    for col in range(7): # go through every element in the matrix
         
-        if row % 2 == 0:
-            if testmap[row][col] != 1 and testmap[row][col] != 0:
+        # check if it is connected horizontally
+        if row % 2 == 0: # if row = 0, 2, 4 
+            if testmap[row][col] != 1 and testmap[row][col] != 0: # check if it is a room
                 print('[ROOM' + str(testmap[row][col] - 1) + ']', end='')
             else:
-                if testmap[row][col] == 1:
+                if testmap[row][col] == 1: # connected
                     print('----', end='')
                 else:
-                    print('    ', end='')
-
+                    print('    ', end='') # disconnect
+        # vertically
         else:
             if testmap[row][col] == 1:
                 print('  |||  ', end='')
             
-            if testmap[row][col] == 0:
+            if testmap[row][col] == 0:  # leave more empty spaces if it is the first element
                 if col == 0:
                     print('       ', end='')
                 
