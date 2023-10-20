c87c88d443a9756a5f22bca43dec94761d8282f0
Lavanya Kasturia <KasturiaL@cardiff.ac.uk>
2023-10-20 21:39:04 +0100

    added score attribute and refactored file formatting

--- player.py
+++ player.py
@@ -1,10 +1,10 @@
-
 class Player:
-	'''
-	This class holds values for the player such as the players name, inventory and current room
-	'''
+    """
+    This class holds values for the player such as the players name, inventory and current room
+    """
 
-	def __init__(self, name, entrance):	#run when a player object is created
-		self.name = name	#string
-		self.current_room = entrance	#room object
-		self.inventory = []		#list of item objects
+    def __init__(self, name, entrance):  # run when a player object is created
+        self.name = name  # string
+        self.current_room = entrance  # room object
+        self.inventory = []  # list of item objects
+        self.score = None  # int
