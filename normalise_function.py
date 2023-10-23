f74645bbc08f9a931281f532b65651655cfb6689
Joel Saju <sajuj1@cardiff.ac.uk>
2023-10-23 10:18:30 +0000

    I wasn't sure how to put it into the gameparser so I created a new .py can someone add it to the main on gameparser.py

---
+++ normalise_function.py
@@ -0,0 +1,28 @@
+import string
+# Function to normalise player input
+def normalise_input(user_input, whitelist):
+    # Step 1: Remove leading and trailing spaces and make it lowercase
+    user_input = user_input.strip()
+    user_input = user_input.lower()
+
+    # Step 2: Remove punctuation
+    for char in string.punctuation:
+        user_input = user_input.replace(char, '')
+
+    # Step 3: Split the input into words
+    words = user_input.split()
+
+    # Step 4: Keep only words in the whitelist
+    normalised_words = []
+    for word in words:
+        if word in whitelist:
+            normalised_words.append(word)
+
+    return normalised_words
+
+
+# Example usage:
+whitelist = ["go", "take", "north", "east", "west", "south"]
+user_input = "Go north and take the key!"
+normalised_input = normalise_input(user_input, whitelist)
+print(normalised_input)
